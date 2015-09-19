f350e762eb4b1d41bd30170f12da82cf2de5f7c0
Martin LAURENT <Martin.LAURENT@gmail.com>
2015-09-19 20:09:40 +0200

    Initialize a process to read Music XML files

---
+++ Sources/readMusicXML.m
@@ -0,0 +1,6 @@
+% readMusicXML 
+octaveLabel = 'octave';
+stepLabel = 'step';
+
+filepath = 'Data/sons/Blue_Orchid_sans_dead_note_avec_bend';
+node= xmlread(strcat(filepath, '\Blue_Orchid_sans_dead_note_avec_bend.xml'));
