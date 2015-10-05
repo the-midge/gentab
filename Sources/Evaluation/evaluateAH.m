29938c6c99be1baf5acb345ab0e13c1e27e06d4f
Martin <martin.laurent30@gmail.com>
2015-10-05 15:31:16 +0200

    Ajoute 3 script vierges pour implémentation

---
+++ Sources/Evaluation/evaluateAH.m
@@ -0,0 +1,8 @@
+%evaluateOD.m
+% 
+%     DESCRIPTION:
+%       Ce script evalue l'analyse harmonique des onsets en lisant le
+%       fichier expected.txt
+%   
+%     BUT:   
+%       ?
---
+++ Sources/Evaluation/evaluateOD.m
@@ -0,0 +1,8 @@
+%evaluateOD.m
+% 
+%     DESCRIPTION:
+%       Ce script evalue le taux de détection des onsets en lisant le
+%       fichier expected.txt
+%   
+%     BUT:   
+%       ?
