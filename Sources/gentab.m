d8646c20457b91981abe7a2ff66f1783ebd1e8fd
Romain <romain.gallard@reseau.eseo.fr>
2015-10-06 08:28:07 +0200

    Changement du chemin d'accès pour avoir les sous dossiers de '/sources'. Changement de la fonction Wavread en audioread, suppression de l'argument Nbits (pas utilisable pour audioread).Supression des fichiers audios inutiles, ajout de 'essaiRomain'

--- Sources/gentab.m
+++ Sources/gentab.m
@@ -9,69 +9,42 @@ close all
 clc
 beep off
 
-addpath(genpath('..\sources')); %Permet l'accès à tous les fichiers du dossier sources
+addpath(genpath('../Sources'))
 
 %% Chargement des données
 disp('Fichier audio en entrée?');
-disp('1: Day Tripper - 8 sec');
-disp('2: Notes variées - 34 sec');
-disp('3: Mélange notes et silences - 12s');
-disp('5: Aller-retour chromatique - 18s');
-disp('6: Blue Orchid (bends) - 30s');
-disp('7: Mad World (intro) - 33s');
-disp('9: Sortie');
+disp('1: essaiRomain');
+disp('2: Blue Orchid (bends) - 30s');
+disp('3: essaiRomain - 12s');
+disp('4: Sortie');
 
 choixEchantillon=input('Choix? '); %Attend une action utilisateur
 clc
 switch choixEchantillon
     case 1
-        disp('Day Tripper');
+        disp('essaiRomain');
         % On selectionne Les 8 premières secondes de la chanson Day Tripper des
         % Beatles
         % Dans cet échantillon, de la guitare est jouée en solo
-        relativePath = '\DATA\sons\DayTripper\';
-        audioFilename='DayTripper.wav';
-        [x,Fs,Nbits]=wavread(audioFilename);
-        x=x(1:Fs*8,1);
+        audioFilename='essaiRomain.wav';
+        [x,Fs]=audioread(audioFilename);
+        x=x(1:Fs*12,1);
     case 2
-        disp('Notes Variées');
+        disp('Blue Orchid (bends)');
         %Où un echantillon généré logiciellement contenant de la guitare et des
         %durées de notes variées.
         audioFilename='Echantillon_34SecondesNotesVariees.wav';
-        [x,Fs,Nbits]=wavread(audioFilename);
-        x=x(1:Fs*34,1);
+        [x,Fs]=audioread(audioFilename);
+        x=x(1:Fs*30,1);
     case 3
-        disp('Notes et silences');
+        disp('essaiRomain');
         %Où un echantillon (12s) généré logiciellement contenant de la guitare et des
         %silences (croches et noires et à la fin un silence d'une ronde et demie
-        audioFilename='silences.wav';
-        [x,Fs,Nbits]=wavread(audioFilename);
+        audioFilename='essaiRomain.wav';
+        [x,Fs]=audioread(audioFilename);
         x=x(1:Fs*12,1);
+    
     case 4
-
-    case 5
-        disp('Aller-Retour chromatique');
-        % Toutes les notes de E2 à A4 sont jouées avec environ le même
-        % intervalle entre chaque (croches).
-        audioFilename='aller-retour-chromatique.wav';
-        [x,Fs,Nbits]=wavread(audioFilename); 
-        x=x(:,1);
-    case 6
-        disp('Blue Orchid');
-        % Enregistrement en guitare claire d'un riff complet de la chanson
-        % Blue Orchid des White Stripes
-        relativePath = 'Data/sons/BlueOrchidSansDeadNoteAvecBend';
-        audioFilename='BlueOrchidSansDeadNoteAvecBend.wav';
-        [x,Fs,Nbits]=wavread(audioFilename);
-        x=x(:,1);
-    case 7
-        disp('Mad World');
-        % Enregistrement en guitare claire d'un arpège de l'intro de Mad
-        % World de Gary Jules
-        audioFilename='garyJulesMadWorldAcousticIntro.wav';
-        [x,Fs,Nbits]=wavread(audioFilename); 
-        x=x(:,1);
-    case 9
         clc
         clear all;
         break;
