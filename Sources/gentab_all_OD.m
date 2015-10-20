b125e21550b4110db89939b8f43b6516f64b69f1
RedDogg <romain.gallard@reseau.eseo.fr>
2015-10-20 14:50:28 +0200

    Fonction pour faire les OD de tous les fichiers audios

---
+++ Sources/gentab_all_OD.m
@@ -0,0 +1,85 @@
+%%%
+% gentab.m
+% Ce script teste tous les composants de l'algorithme à partir 
+% d'un signal audio qu'il charge lui même
+%
+
+clear all
+close all
+clc
+beep off
+
+addpath(genpath('../Sources'))
+
+
+%% Prétraitement
+% TODO:
+%   Intégrer ici des traitements sur le signal qui doivent être exécutés
+%   avant toute opérations.
+
+%% Éxécution
+%   Réquête utilisateur et exécution de tout où partie de l'algorithme
+
+
+disp('OD: Onset Detection des fichiers audios');
+disp('OUT: Sortie');
+
+OD='OD';
+
+OUT='OUT';
+choixAlgo=input('Choix? ');
+
+clc
+
+%% Onset Detection
+if(~strcmp(choixAlgo, OUT)) % Dans tout les cas sauf une sortie
+        audioFilename='figRythmique.wav';
+        [x,Fs]=audioread(audioFilename);
+        x=x(:,1);
+        OnsetDetection;
+        title(audioFilename);
+        audioFilename='guitar1.wav';
+        [x,Fs]=audioread(audioFilename);
+        x=x(:,1);
+        OnsetDetection;
+        title(audioFilename);
+        audioFilename='heart-and-soul-tux.wav';
+        [x,Fs]=audioread(audioFilename);
+        x=x(:,1);
+        OnsetDetection;
+        title(audioFilename);
+        audioFilename='ar-diatonique-tux.wav';
+        [x,Fs]=audioread(audioFilename);
+        x=x(:,1);
+        OnsetDetection;
+        title(audioFilename);
+        audioFilename='DayTripper.wav';
+        [x,Fs]=audioread(audioFilename);
+        x=x(1:Fs*8,1);
+        OnsetDetection; 
+        title(audioFilename);
+        audioFilename='BlueOrchidSansDeadNoteAvecBend.wav';
+        [x,Fs]=audioread(audioFilename);
+        x=x(1:Fs*30,1);
+        OnsetDetection; 
+        title(audioFilename);
+        audioFilename='guitar2.wav';
+        [x,Fs]=audioread(audioFilename);
+        x=x(:,1);
+        OnsetDetection; 
+        title(audioFilename);
+        audioFilename='guitar3.wav';
+        [x,Fs]=audioread(audioFilename);
+        x=x(:,1);
+        OnsetDetection; 
+        title(audioFilename);
+end
+
+
+if(strcmp(choixAlgo, OUT))
+    clc
+    close all
+    clear all
+end
+clear OD OUT;
+
