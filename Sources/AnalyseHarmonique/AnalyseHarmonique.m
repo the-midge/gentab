8d614e322d54f05afe72f967459d9dfb2a42f753
Romain <romain.gallard@reseau.eseo.fr>
2015-11-26 11:57:26 +0100

    Meilleure méthode pour l'instant d'AH

--- Sources/AnalyseHarmonique/AnalyseHarmonique.m
+++ Sources/AnalyseHarmonique/AnalyseHarmonique.m
@@ -5,7 +5,7 @@
 %       segment par segment, incluant l'octave de la note.
 disp('Début identification des notes');
 for index= [1:length(segments)]
-    notesJouee(index,:)=find_second_peak(segments{index} , Fs);
+    notesJouee(index,:)=determinationNoteSegmentOctave_Harmonic_Product_Spectrum(segments{index} , Fs);
 end
 disp('Fin identification des notes');
 
