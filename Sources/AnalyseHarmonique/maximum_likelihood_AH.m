9d11368a43766dc237f235dcab000be08a035d56
= <martin.laurent30@gmail.com>
2015-11-30 11:10:44 +0100

    Décommente un partie de spectral flux
    On retrouve ainsi les bons résultats.

---
+++ Sources/AnalyseHarmonique/maximum_likelihood_AH.m
@@ -0,0 +1,6 @@
+% maximum_likelihood_AH.m
+%   Ce script impléménte la méthode décrite dans la publication
+%   "Mximum likelihood Pitch Estimation Using Sinusoidal Modeling", de V.
+%   Mahadevan.
+
+s=x;
