17efefc9a41f1935f61813fe8cd8d5dae609157e
Romain <romain.gallard@reseau.eseo.fr>
2016-01-20 16:07:51 +0100

    Interface graphique

--- Sources/Application/build-GenTab-Desktop_Qt_5_5_1_clang_64bit-Debug/ui_MainWindow.h
+++ Sources/Application/build-GenTab-Desktop_Qt_5_5_1_clang_64bit-Debug/ui_MainWindow.h
@@ -16,6 +16,8 @@
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QRadioButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTextEdit>
 #include <QtWidgets/QWidget>
@@ -27,6 +29,15 @@ class Ui_MainWindow
 public:
     QWidget *centralWidget;
     QTextEdit *textEdit;
+    QTextEdit *textEdit_2;
+    QTextEdit *textEdit_3;
+    QTextEdit *textEdit_4;
+    QTextEdit *textEdit_5;
+    QRadioButton *radioButton;
+    QRadioButton *radioButton_2;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
     QMenuBar *menuBar;
     QStatusBar *statusBar;
 
@@ -34,20 +45,101 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->setWindowModality(Qt::WindowModal);
+        MainWindow->setWindowModality(Qt::NonModal);
+        MainWindow->setEnabled(true);
         MainWindow->resize(1280, 720);
         MainWindow->setStyleSheet(QStringLiteral("background: url(/Users/apple/Desktop/Gentab_projet/Sources/Application/GenTab/guitar.jpeg)"));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         textEdit = new QTextEdit(centralWidget);
         textEdit->setObjectName(QStringLiteral("textEdit"));
-        textEdit->setGeometry(QRect(430, 0, 441, 91));
+        textEdit->setEnabled(false);
+        textEdit->setGeometry(QRect(430, 0, 421, 51));
+        textEdit->setAutoFillBackground(false);
         textEdit->setStyleSheet(QLatin1String("color: rgb(255, 17, 29);\n"
 "font: 75 13pt \"Georgia\";\n"
 "\n"
 ""));
         textEdit->setOverwriteMode(true);
+        textEdit_2 = new QTextEdit(centralWidget);
+        textEdit_2->setObjectName(QStringLiteral("textEdit_2"));
+        textEdit_2->setEnabled(false);
+        textEdit_2->setGeometry(QRect(100, 130, 171, 31));
+        textEdit_2->setAutoFillBackground(false);
+        textEdit_2->setStyleSheet(QLatin1String("color: rgb(255, 17, 29);\n"
+"font: 75 13pt \"Georgia\";\n"
+"\n"
+""));
+        textEdit_2->setOverwriteMode(true);
+        textEdit_3 = new QTextEdit(centralWidget);
+        textEdit_3->setObjectName(QStringLiteral("textEdit_3"));
+        textEdit_3->setEnabled(false);
+        textEdit_3->setGeometry(QRect(100, 370, 171, 31));
+        textEdit_3->setAutoFillBackground(true);
+        textEdit_3->setStyleSheet(QLatin1String("color: rgb(255, 17, 29);\n"
+"font: 75 13pt \"Georgia\";\n"
+"\n"
+""));
+        textEdit_3->setOverwriteMode(true);
+        textEdit_4 = new QTextEdit(centralWidget);
+        textEdit_4->setObjectName(QStringLiteral("textEdit_4"));
+        textEdit_4->setEnabled(false);
+        textEdit_4->setGeometry(QRect(860, 130, 171, 31));
+        textEdit_4->setAutoFillBackground(false);
+        textEdit_4->setStyleSheet(QLatin1String("color: rgb(255, 17, 29);\n"
+"font: 75 13pt \"Georgia\";\n"
+"\n"
+""));
+        textEdit_4->setOverwriteMode(true);
+        textEdit_5 = new QTextEdit(centralWidget);
+        textEdit_5->setObjectName(QStringLiteral("textEdit_5"));
+        textEdit_5->setEnabled(false);
+        textEdit_5->setGeometry(QRect(860, 370, 171, 31));
+        textEdit_5->setAutoFillBackground(false);
+        textEdit_5->setStyleSheet(QLatin1String("color: rgb(255, 17, 29);\n"
+"font: 75 13pt \"Georgia\";\n"
+"\n"
+""));
+        textEdit_5->setOverwriteMode(true);
+        radioButton = new QRadioButton(centralWidget);
+        radioButton->setObjectName(QStringLiteral("radioButton"));
+        radioButton->setEnabled(true);
+        radioButton->setGeometry(QRect(1050, 180, 100, 20));
+        radioButton->setStyleSheet(QStringLiteral("color: rgb(186, 186, 186);"));
+        radioButton_2 = new QRadioButton(centralWidget);
+        radioButton_2->setObjectName(QStringLiteral("radioButton_2"));
+        radioButton_2->setEnabled(true);
+        radioButton_2->setGeometry(QRect(1050, 220, 100, 20));
+        radioButton_2->setStyleSheet(QStringLiteral("color: rgb(186, 186, 186);"));
+        pushButton = new QPushButton(centralWidget);
+        pushButton->setObjectName(QStringLiteral("pushButton"));
+        pushButton->setEnabled(true);
+        pushButton->setGeometry(QRect(290, 180, 113, 32));
+        pushButton->setAutoFillBackground(false);
+        pushButton->setStyleSheet(QStringLiteral("color: rgb(186, 186, 186);"));
+        pushButton_2 = new QPushButton(centralWidget);
+        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
+        pushButton_2->setEnabled(true);
+        pushButton_2->setGeometry(QRect(290, 430, 113, 32));
+        pushButton_2->setAutoFillBackground(false);
+        pushButton_2->setStyleSheet(QStringLiteral("color: rgb(186, 186, 186);"));
+        pushButton_3 = new QPushButton(centralWidget);
+        pushButton_3->setObjectName(QStringLiteral("pushButton_3"));
+        pushButton_3->setEnabled(true);
+        pushButton_3->setGeometry(QRect(1060, 420, 113, 32));
+        pushButton_3->setAutoFillBackground(false);
+        pushButton_3->setStyleSheet(QStringLiteral("color: rgb(186, 186, 186);"));
         MainWindow->setCentralWidget(centralWidget);
+        textEdit->raise();
+        textEdit_2->raise();
+        textEdit_3->raise();
+        textEdit_4->raise();
+        textEdit_5->raise();
+        radioButton_2->raise();
+        radioButton->raise();
+        pushButton->raise();
+        pushButton_2->raise();
+        pushButton_3->raise();
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
         menuBar->setGeometry(QRect(0, 0, 1280, 22));
@@ -69,6 +161,34 @@ public:
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'Georgia'; font-size:13pt; font-weight:72; font-style:normal;\">\n"
 "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:36pt; font-weight:600; color:#a71819;\">Bienvenue sur Gentab</span></p></body></html>", 0));
+        textEdit_2->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Georgia'; font-size:13pt; font-weight:72; font-style:normal;\">\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:18pt; font-weight:600; color:#a71819;\">Audacity project</span></p></body></html>", 0));
+        textEdit_3->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Georgia'; font-size:13pt; font-weight:72; font-style:normal;\">\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:18pt; font-weight:600; color:#a71819;\">Wave file</span></p></body></html>", 0));
+        textEdit_4->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Georgia'; font-size:13pt; font-weight:72; font-style:normal;\">\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:18pt; font-weight:600; color:#a71819;\">Export to</span></p></body></html>", 0));
+        textEdit_5->setHtml(QApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'Georgia'; font-size:13pt; font-weight:72; font-style:normal;\">\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:18pt; font-weight:600; color:#a71819;\">File name</span></p></body></html>", 0));
+#ifndef QT_NO_TOOLTIP
+        radioButton->setToolTip(QApplication::translate("MainWindow", "<html><head/><body><p><br/></p></body></html>", 0));
+#endif // QT_NO_TOOLTIP
+        radioButton->setText(QApplication::translate("MainWindow", "     MIDI", 0));
+        radioButton_2->setText(QApplication::translate("MainWindow", "     GP4", 0));
+        pushButton->setText(QApplication::translate("MainWindow", "Open", 0));
+        pushButton_2->setText(QApplication::translate("MainWindow", "Open", 0));
+        pushButton_3->setText(QApplication::translate("MainWindow", "Open", 0));
     } // retranslateUi
 
 };
